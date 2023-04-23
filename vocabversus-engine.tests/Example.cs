a4f6bbb574a58abfc46d76ffd7403f9625a45796
Thomas Molen <sugarypineapple2@gmail.com>
2023-04-23 18:08:40 +0200

    Added testing framework and testing CI
    This adds automated unit testing to the CI/CD pipeline using XUnit

---
+++ vocabversus-engine.tests/Example.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace vocabversus_engine.tests
+{
+    public class Example
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
